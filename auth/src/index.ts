29ed8e5a842bd4c1dc4cc64de2d1289f8f1b9d5f
yamen <yamen225@gmail.com>
2021-02-13 18:25:20 +0200

    connect to mongoose and mongo db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -22,6 +23,21 @@ app.all('*', async (req, res, next) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongo DB');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+start();
