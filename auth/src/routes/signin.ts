f1ebde1363781c9e7e205a59ab13606752fed9b0
yamen <yamen225@gmail.com>
2021-02-15 21:20:22 +0200

    finish signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,47 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { ValidateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/users';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  ValidateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY!
+    );
+    //store it inside the session object
+    req.session = {
+      jwt: userJwt,
+    };
 
-export {router as signinRouter};
+    res.status(200).send(existingUser);
+  }
+);
+
+export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { User } from '../models/users';
-import { RequestValidationError } from '../errors/requestValidationError';
+import { ValidateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
+
 import jwt from 'jsonwebtoken';
 
 const router = express.Router();
@@ -16,13 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20'),
   ],
+  ValidateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
