508d66d6ee3af10fe3ed2d521c8093ea7797deac
yamen <yamen225@gmail.com>
2021-02-15 23:45:04 +0200

    finish signout flow

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    res.send({ currentUser: null });
-  }
-  try {
-    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch {
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
