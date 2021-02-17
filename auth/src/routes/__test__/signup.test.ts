ab8e4557229523b375a940bf1a8041dd218c74c5
yamen <yamen225@gmail.com>
2021-02-18 01:40:21 +0200

    test auth service routes

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+});
+it('returns 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: 'testtest.com', password: 'password' })
+    .expect(400);
+});
+it('returns 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'pa' })
+    .expect(400);
+});
+it('returns 400 with an empty email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+});
+it('dissalows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(400);
+});
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201);
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
